69c2ffc9fb2367a26473fef7fa3d5e32717a13dd
d4nQw3rty <danlinmoy@gmail.com>
2022-10-06 10:49:11 -0300

    Create storageMethods.test.js
    -Create test for adding element to local storage
    -Test if element is added and if storage recieve an empty element

---
+++ src/modules/storageMethods.test.js
@@ -0,0 +1,26 @@
+/** * @jest-environment jsdom */
+
+import Methods from './storageMethods.js';
+
+window.localStorage = Storage.prototype;
+document.body.innerHTML = '<ol id="list"></ol>';
+
+describe('Add task feature', () => {
+   
+  test('correct adding feature', () => {
+  let tasks = Methods.add('Task 1');
+  expect(tasks).toHaveLength(1);
+  tasks = Methods.add('Task 2');
+  expect(tasks).toHaveLength(2);
+});
+
+test('Empty task description', () => {
+  const tasks = Methods.add('');
+  expect(tasks).toBe(-1);
+});
+
+test('Add <li> elements', () => {
+  const htmlTasks = document.querySelectorAll('.list-item');
+  expect(htmlTasks).toHaveLength(2);
+});
+});
