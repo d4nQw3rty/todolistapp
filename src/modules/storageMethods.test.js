39d89d1405d66321b8647d9f5ca4cbf6ac6453d7
MAHDIA001 <mahdianaiebi@gmail.com>
2022-10-06 20:22:18 +0500

    add script to test deleting function

--- src/modules/storageMethods.test.js
+++ src/modules/storageMethods.test.js
@@ -3,24 +3,32 @@
 import Methods from './storageMethods.js';
 
 window.localStorage = Storage.prototype;
-document.body.innerHTML = '<ol id="list"></ol>';
+document.body.innerHTML = '<ol id='list'></ol>';
 
 describe('Add task feature', () => {
-   
   test('correct adding feature', () => {
-  let tasks = Methods.add('Task 1');
-  expect(tasks).toHaveLength(1);
-  tasks = Methods.add('Task 2');
-  expect(tasks).toHaveLength(2);
+    let tasks = Methods.add('Task 1');
+    expect(tasks).toHaveLength(1);
+    tasks = Methods.add('Task 2');
+    expect(tasks).toHaveLength(2);
 });
 
-test('Empty task description', () => {
-  const tasks = Methods.add('');
-  expect(tasks).toBe(-1);
+  test('Empty task description', () => {
+    const tasks = Methods.add('');
+    expect(tasks).toBe(-1);
 });
 
-test('Add <li> elements', () => {
-  const htmlTasks = document.querySelectorAll('.list-item');
-  expect(htmlTasks).toHaveLength(2);
+  test('Add <li> elements', () => {
+    const htmlTasks = document.querySelectorAll('.list-item');
+    expect(htmlTasks).toHaveLength(2);
+  });
 });
+describe('Remove task feature', () => {
+  test('correct removing feature', () => {
+    let htmlTasks = document.querySelectorAll('.list-item');
+    expect(htmlTasks).toHaveLength(2);
+    Methods.remove(1);
+    htmlTasks = document.querySelectorAll('.list-item');
+    expect(htmlTasks).toHaveLength(1);
+  });
 });
