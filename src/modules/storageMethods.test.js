101416015bc428afd1e78230304e5cc5457137c0
d4nQw3rty <danlinmoy@gmail.com>
2022-10-06 12:08:12 -0300

    Fix linters

--- src/modules/storageMethods.test.js
+++ src/modules/storageMethods.test.js
@@ -3,7 +3,7 @@
 import Methods from './storageMethods.js';
 
 window.localStorage = Storage.prototype;
-document.body.innerHTML = '<ol id='list'></ol>';
+document.body.innerHTML = '<ol id="list"></ol>';
 
 describe('Add task feature', () => {
   test('correct adding feature', () => {
@@ -11,18 +11,19 @@ describe('Add task feature', () => {
     expect(tasks).toHaveLength(1);
     tasks = Methods.add('Task 2');
     expect(tasks).toHaveLength(2);
-});
+  });
 
   test('Empty task description', () => {
     const tasks = Methods.add('');
     expect(tasks).toBe(-1);
-});
+  });
 
   test('Add <li> elements', () => {
     const htmlTasks = document.querySelectorAll('.list-item');
     expect(htmlTasks).toHaveLength(2);
   });
 });
+
 describe('Remove task feature', () => {
   test('correct removing feature', () => {
     let htmlTasks = document.querySelectorAll('.list-item');
